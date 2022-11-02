f821937f97e833dee91062f743ddb35062fd3662
Nastya <himeriya97@mgmail.com>
2022-11-02 18:24:02 +0300

    feat: add decision to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.mkdir(path.join(__dirname,'files-copy'), {recursive: true}, (err)=>{
+  if (err) throw err;
+  fs.promises.readdir(path.join(__dirname,'files-copy'), {withFileTypes: true})
+    .then(filenames=> {
+      if (filenames.length > 0){
+        for (let file of filenames){
+          fs.unlink(path.join(__dirname,'files-copy', file.name), (err)=>{if (err) throw err})
+        }
+      }
+      
+
+      fs.promises.readdir(path.join(__dirname,'files'),{withFileTypes: true})
+      .then(filenames => {
+        for (let file of filenames){
+          if (!file.isDirectory()){
+            let filePath = path.join(__dirname, 'files', file.name);
+            fs.copyFile(filePath, path.join(__dirname,'files-copy',file.name), (err)=>{
+              if (err) throw err;
+            })
+          }
+        }
+      });
+    })
+});
 
